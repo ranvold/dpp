069b467bcc6abd1c1e18815b62fd3b4d7aea5ead
Volodymyr Shafran <shafran.valdemarovich@gmail.com>
2023-04-27 12:19:39 +0300

    use try_lock to avoid deadlock

--- main.cpp
+++ main.cpp
@@ -14,32 +14,33 @@ mutex forks[N]; // виделки
 void philosopher(int id) {
     int left = id;
     int right = (id + 1) % N;
-
-    // визначаємо час
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> sleep_dist(500, 1500);
     
     while (true) {
         // думаємо
         cout << "Філософ " << id << " думає" << endl;
-        this_thread::sleep_for(chrono::milliseconds(sleep_dist(gen)));
+        this_thread::sleep_for(chrono::milliseconds(1000));
         
-        // беремо виделки
+        // пробуємо взяти виделки
         cout << "Філософ " << id << " бере ліву виделку " << left << endl;
         forks[left].lock();
+
         cout << "Філософ " << id << " бере праву виделку " << right << endl;
-        forks[right].lock();
-        
-        // їмо
-        cout << "Філософ " << id << " їсть" << endl;
-        this_thread::sleep_for(chrono::milliseconds(sleep_dist(gen)));
-        
-        // кладемо виделки
-        cout << "Філософ " << id << " кладе ліву виделку " << left << endl;
-        forks[left].unlock();
-        cout << "Філософ " << id << " кладе праву виделку " << right << endl;
-        forks[right].unlock();
+        if (forks[right].try_lock()) {
+          // їмо
+          cout << "Філософ " << id << " їсть" << endl;
+          this_thread::sleep_for(chrono::milliseconds(1000));
+
+          // кладемо виделки
+          cout << "Філософ " << id << " кладе ліву виделку " << left << endl;
+          forks[left].unlock();
+          cout << "Філософ " << id << " кладе праву виделку " << right << endl;
+          forks[right].unlock();
+        }
+        else {
+          // кладемо ліву виделку
+          cout << "Філософ " << id << " кладе ліву виделку " << left << endl;
+          forks[left].unlock();
+        }
     }
 }
 
